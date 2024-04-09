f16bf4861588e95ec939f7f3977c35b857486fd9
TheNewLearner1 <161268365+TheNewLearner1@users.noreply.github.com>
2024-04-09 22:18:52 +0545

    dwde

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,6 +1,10 @@
 fn main() {
     let mut _x: i32 = 10;
     if _x == 10 {
-        println!("{}", _x);
+        println!("{_x}");
     }
-}
+    let tup: (i32, i32, i32) = (1,2,3);
+    let (x,y,z) = tup;
+    let xo = tup.0;
+    println!("the valeu of y is {xo}");
+} 

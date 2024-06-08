3dd58b6bc8e04b76c26ef94f962c4f7fb2c048d0
bob <zzzooozzz836@gmail.com>
2024-06-08 22:04:15 +0545

    what the hell

--- what/src/main.rs
+++ what/src/main.rs
@@ -1,25 +1,20 @@
 fn main() {
-    let mut listo: [i32, 5]  = [221, 466, 110, 945, 864];
-    let mut max_lenth: usize = 0;
-    for _text in listo {
-        max_lenth += 1
-    }
-    max_lenth -= 1;
-
+    let mut listo: [i32; 5] = [221, 466, 110, 945, 864];
+    let max_lenth: usize = listo.len() - 1;
     let mut complete: usize = 0;
-    let right: usize = max_lenth;
     let mut left: usize = 0;
-    while complete < max_lenth {
-        let mut value = listo[complete];
-        while left < right {
-            left = complete;
+    let mut value = listo[complete];
+    while complete <= max_lenth {
+        while left < complete {
+        left = complete;
             if listo[left] < value {
                 value = listo[left];
-                left += 1;
             }
+            left += 1;
         }
         listo[complete] = value;
         complete += 1;
+        value = 100000;
     }
     for text in listo {
         print!("{text}, ");

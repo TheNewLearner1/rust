ddcb4b0b29454e8ae2ec1d8ab74f1ff43a1cbbd5
zzzz <zzzooozzz836@gmail.com>
2024-07-05 23:18:47 +0545

    whatdcsfsd

--- stats/src/main.rs
+++ stats/src/main.rs
@@ -8,9 +8,17 @@ fn main() {
     }
 
     println!(" number of iternation ");
-
     let mut iternation = String::new();
     stdin().read_line(&mut iternation).unwrap();
-    println!("iternation {iternation} ");
+    println!("start point");
+    iternation = iternation.to_string();
+    let mut point = String::new();
+    stdin().read_line(&mut iternation).unwrap();
+
+    println!("difference ");
+    let mut dif = String::new();
+    stdin().read_line(&mut iternation).unwrap();
+    let mut x = 1;
+    while x <= iternation {}
 }
 fn mean_deviation() {}
